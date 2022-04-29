3b865ef874e2ac74472ef740ebd8f1fd313af1d2
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2022-04-29 11:03:33 +0100

    Added build for github actions  Failed testing

--- backend/src/test/java/com/kmvpsolutions/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kmvpsolutions/backend/BackendApplicationTests.java
@@ -1,13 +1,15 @@
 package com.kmvpsolutions.backend;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        Assertions.fail("Some random message test error");
+    }
 
 }
