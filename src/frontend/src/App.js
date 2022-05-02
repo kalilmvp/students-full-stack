f96688691804dfb1e2b64d470594c1c95cce1b51
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2022-05-02 10:42:52 +0100

    test 04

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -223,7 +223,7 @@ function App() {
                         rel="noopener noreferrer"
                         target="_blank"
                         href="https://amigoscode.com/p/full-stack-spring-boot-react">
-                        Click here to access Fullstack Spring Boot & React for professionals
+                        Click here to access Fullstack Spring Boot & React for professionals 11111
                     </a>
                 </Divider>
             </Footer>
--- src/test/java/com/kmvpsolutions/backend/BackendApplicationTests.java
+++ src/test/java/com/kmvpsolutions/backend/BackendApplicationTests.java
@@ -1,6 +1,5 @@
 package com.kmvpsolutions.backend;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
