6c2a19608aa0dcb4a05d9fd7cdae58f770a27eaa
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2022-05-05 22:19:27 +0100

    Added integrations tests

--- src/main/java/com/kmvpsolutions/backend/student/repositories/StudentRepository.java
+++ src/main/java/com/kmvpsolutions/backend/student/repositories/StudentRepository.java
@@ -13,4 +13,5 @@ import org.springframework.stereotype.Repository;
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
     boolean existsStudentByEmail(String email);
+    Student findStudentByEmail(String mail);
 }
--- src/test/java/com/kmvpsolutions/backend/student/services/StudentServiceTest.java
+++ src/test/java/com/kmvpsolutions/backend/student/services/StudentServiceTest.java
@@ -5,14 +5,11 @@ import com.kmvpsolutions.backend.exceptions.StudentNotFoundException;
 import com.kmvpsolutions.backend.student.model.Gender;
 import com.kmvpsolutions.backend.student.model.Student;
 import com.kmvpsolutions.backend.student.repositories.StudentRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Sort;
 
@@ -20,9 +17,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
